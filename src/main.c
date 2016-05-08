f530391db4a03494b35278031ccbd4dee77d87e9
Henri Buyse <henri.buyse@gmail.com>
2016-05-08 15:39:40 +0200

    Basis

---
+++ src/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file: main.c
+ * @author: hbuyse
+ */
+ 
+#include <stdio.h>
+
+
+int main(int        argc,
+         char const *argv[]
+         )
+{
+    printf("Hello World!");
+
+    return (0);
+}
