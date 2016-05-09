92cd185285a33b6d880b4d81c04e5da51ca35e2e
Henri Buyse <henri.buyse@eolane.com>
2016-05-09 16:29:34 +0200

    Update main

--- src/main.c
+++ src/main.c
@@ -3,12 +3,21 @@
  * @author: hbuyse
  */
 
-#include <stdio.h>
-
+#include <user.h>
+#include <http_code.h>
 
 int main(void)
 {
-    printf("Hello World!\n");
+    PB_user_t           user;
+    unsigned char       res = 0;
+
+
+    res = pb_get_user(&user, "0000000000000000000000000000000000");
+
+    if ( res == HTTP_OK )
+    {
+        dump_user_info(user);
+    }
 
     return (0);
 }
