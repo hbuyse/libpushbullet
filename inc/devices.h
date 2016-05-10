530df5fc602a1d9cf513638cf24ee2199a53e7a8
Henri Buyse <henri.buyse@eolane.com>
2016-05-10 15:40:19 +0200

    Devices string informations are constant

--- inc/devices.h
+++ inc/devices.h
@@ -11,46 +11,47 @@
 
 #include <json/json.h>
 
+#include <user.h>          // PB_user_t
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-
 typedef struct {
     unsigned char active;          // true,
-    char *iden;          // "ujEIL5AaxhYsjAyl73vD3s",
+    const char *iden;          // "ujEIL5AaxhYsjAyl73vD3s",
     double created;          // 1459865409.299245,
     double modified;          // 1459865409.2992482,
-    char *type;          // "android",
-    char *kind;          // "android",
-    char *nickname;          // "LGE Nexus 5X",
+    const char *type;          // "android",
+    const char *kind;          // "android",
+    const char *nickname;          // "LGE Nexus 5X",
     unsigned char generated_nickname;          // true,
-    char *manufacturer;          // "LGE",
-    char *model;          // "Nexus 5X",
+    const char *manufacturer;          // "LGE",
+    const char *model;          // "Nexus 5X",
     short app_version;          // 256,
     json_object *fingerprint;          // "{\"android_id\":\"f48298904b847bd0\"}",
-    char *push_token;          // "fcH5ndd7TuY:APA91bHaNqTASWFRP7dOg5YUfEhuTNv1AB-zBljo_mHzB-zUtUxAPtYkEUwKz69V0dG5vWJFhUb52x7sbEwCZbAYjxYpmKMbacfEtMi1Dvm-eqdKWzJEJQzYWTD5lyADztPcFHIRYYEQ",
+    const char *push_token;          // "fcH5ndd7TuY:APA91bHaNqTASWFRP7dOg5YUfEhuTNv1AB-zBljo_mHzB-zUtUxAPtYkEUwKz69V0dG5vWJFhUb52x7sbEwCZbAYjxYpmKMbacfEtMi1Dvm-eqdKWzJEJQzYWTD5lyADztPcFHIRYYEQ",
     unsigned char pushable;          // true,
     unsigned char has_sms;          // true,
     unsigned char has_mms;          // true,
-    char *icon;          // "phone",
-    char *remote_files;          // "disabled"
+    const char *icon;          // "phone",
+    const char *remote_files;          // "disabled"
 } PB_phone_t;
 
 
 typedef struct {
     unsigned char active;          // true,
-    char *iden;          // "ujEIL5AaxhYsjAaqJB5LXg",
+    const char *iden;          // "ujEIL5AaxhYsjAaqJB5LXg",
     double created;          // 1427959507.662864,
     double modified;          // 1427959507.662869,
-    char *type;          // "chrome",
-    char *kind;          // "chrome",
-    char *nickname;          // "Chrome",
-    char *manufacturer;          // "Google",
-    char *model;          // "Chrome",
+    const char *type;          // "chrome",
+    const char *kind;          // "chrome",
+    const char *nickname;          // "Chrome",
+    const char *manufacturer;          // "Google",
+    const char *model;          // "Chrome",
     short app_version;          // 188,
     unsigned char pushable;          // true,
-    char *icon;          // "browser"
+    const char *icon;          // "browser"
 } PB_browser_t;
 
 unsigned short pb_get_devices(PB_user_t user);
