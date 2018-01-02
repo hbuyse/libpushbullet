26fdb42f84cc411333e3d7ba1ab1dfb64e1fb11a
Henri Buyse <henri.buyse@gmail.com>
2018-01-02 14:12:10 +0100

    [test] update recipes and .travis.yml

--- tests/ts_config.c
+++ tests/ts_config.c
@@ -1,208 +1,173 @@
 #include <stdlib.h>
-#include <check.h>
+#include <string.h>
 
 #include "pushbullet.h"
 
-START_TEST (test_pb_config_new)
+static void test_empty_config(void)
 {
     pb_config_t* c = pb_config_new();
 
-    ck_assert( c != NULL );
-    
-    if (c) { free(c); }
-}
-END_TEST
-
-START_TEST (test_pb_config_ref_unref)
-{
-    pb_config_t* c = pb_config_new();
-
-    ck_assert( c != NULL );
-
-    pb_config_ref(c);
-    ck_assert( c != NULL );
-
-    pb_config_unref(c);
-    ck_assert( c != NULL );
-
-    pb_config_unref(c);
-    ck_assert( c != NULL );
+    g_assert( c != NULL );
     
     pb_config_unref(c);
-    ck_assert( c != NULL );
 }
-END_TEST
 
-START_TEST (test_pb_config_https_proxy)
+static void test_https_proxy(void)
 {
     pb_config_t* c = pb_config_new();
     pb_config_t* d = NULL;
 
-    ck_assert( c != NULL );
+    g_assert( c != NULL );
 
-    ck_assert( pb_config_get_https_proxy(c) == NULL );
-    ck_assert( pb_config_get_https_proxy(d) == NULL );
+    g_assert( pb_config_get_https_proxy(c) == NULL );
+    g_assert( pb_config_get_https_proxy(d) == NULL );
 
-    ck_assert_int_eq( pb_config_set_https_proxy(c, "localhost"), 0 );
-    ck_assert_int_eq( pb_config_set_https_proxy(d, "localhost"), -1 );
+    g_assert_cmpint( pb_config_set_https_proxy(c, "localhost"), ==,  0 );
+    g_assert_cmpint( pb_config_set_https_proxy(d, "localhost"), ==,  -1 );
 
-    ck_assert( pb_config_get_https_proxy(c) != NULL );
-    ck_assert( pb_config_get_https_proxy(d) == NULL );
+    g_assert( pb_config_get_https_proxy(c) != NULL );
+    g_assert( pb_config_get_https_proxy(d) == NULL );
 
-    ck_assert_str_eq( pb_config_get_https_proxy(c), "localhost" );
+    g_assert_cmpstr( pb_config_get_https_proxy(c), ==, "localhost" );
 
 
     pb_config_unref(c);
 }
-END_TEST
 
-START_TEST (test_pb_config_timeout)
+static void test_timeout(void)
 {
     pb_config_t* c = pb_config_new();
     pb_config_t* d = NULL;
 
-    ck_assert( c != NULL );
+    g_assert( c != NULL );
 
-    ck_assert_int_eq( pb_config_get_timeout(c), 0 );
-    ck_assert_int_eq( pb_config_get_timeout(d), 0 );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  0 );
+    g_assert_cmpint( pb_config_get_timeout(d), ==,  0 );
 
-    ck_assert_int_eq( pb_config_set_timeout(c, 5), 0 );
-    ck_assert_int_eq( pb_config_set_timeout(d, 0), -1 );
+    g_assert_cmpint( pb_config_set_timeout(c, 5), ==,  0 );
+    g_assert_cmpint( pb_config_set_timeout(d, 0), ==,  -1 );
 
-    ck_assert_int_eq( pb_config_get_timeout(c), 5 );
-    ck_assert_int_eq( pb_config_get_timeout(d), 0 );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  5 );
+    g_assert_cmpint( pb_config_get_timeout(d), ==,  0 );
 
     pb_config_unref(c);
 }
-END_TEST
 
-START_TEST (test_pb_config_token_key)
+static void test_token_key(void)
 {
     pb_config_t* c = pb_config_new();
     pb_config_t* d = NULL;
 
-    ck_assert( c != NULL );
+    g_assert( c != NULL );
 
-    ck_assert( pb_config_get_token_key(c) == NULL );
-    ck_assert( pb_config_get_token_key(d) == NULL );
+    g_assert( pb_config_get_token_key(c) == NULL );
+    g_assert( pb_config_get_token_key(d) == NULL );
 
-    ck_assert_int_eq( pb_config_set_token_key(c, "hello_world"), 0 );
-    ck_assert_int_eq( pb_config_set_token_key(d, "hello_world"), -1 );
+    g_assert_cmpint( pb_config_set_token_key(c, "hello_world"), ==,  0 );
+    g_assert_cmpint( pb_config_set_token_key(d, "hello_world"), ==,  -1 );
 
-    ck_assert( pb_config_get_token_key(c) != NULL );
-    ck_assert( pb_config_get_token_key(d) == NULL );
+    g_assert( pb_config_get_token_key(c) != NULL );
+    g_assert( pb_config_get_token_key(d) == NULL );
 
-    ck_assert_str_eq( pb_config_get_token_key(c), "hello_world" );
+    g_assert_cmpstr( pb_config_get_token_key(c), ==, "hello_world" );
 
 
     pb_config_unref(c);
 }
-END_TEST
 
-START_TEST (test_pb_config_from_json_file)
+static void test_from_json_file(void)
 {
     pb_config_t* c = NULL;
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, NULL), -1);
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, NULL), ==,  -1);
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/"), -1);
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/"), ==,  -1);
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/null.json"), -1);
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/null.json"), ==,  -1);
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/empty.json"), -1);
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/empty.json"), ==,  -1);
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/empty_json.json"), 0);
-    ck_assert( pb_config_get_token_key(c) == NULL );
-    ck_assert( pb_config_get_https_proxy(c) == NULL );
-    ck_assert_int_eq( pb_config_get_timeout(c), 0 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/empty_json.json") , ==, 0);
+    g_assert( pb_config_get_token_key(c) == NULL );
+    g_assert( pb_config_get_https_proxy(c) == NULL );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  0 );
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/empty_values.json"), 0);
-    ck_assert( pb_config_get_token_key(c) != NULL );
-    ck_assert( pb_config_get_https_proxy(c) != NULL );
-    ck_assert_int_eq( strlen(pb_config_get_token_key(c)), 0 );
-    ck_assert_int_eq( strlen(pb_config_get_https_proxy(c)), 0 );
-    ck_assert_int_eq( pb_config_get_timeout(c), 0 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/empty_values.json") , ==, 0);
+    g_assert( pb_config_get_token_key(c) != NULL );
+    g_assert( pb_config_get_https_proxy(c) != NULL );
+    g_assert_cmpint( strlen(pb_config_get_token_key(c)), ==,  0 );
+    g_assert_cmpint( strlen(pb_config_get_https_proxy(c)), ==,  0 );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  0 );
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/proxies.json"), 0);
-    ck_assert( pb_config_get_token_key(c) == NULL );
-    ck_assert( pb_config_get_https_proxy(c) != NULL );
-    ck_assert_int_ne( strlen(pb_config_get_https_proxy(c)), 0 );
-    ck_assert_str_eq( pb_config_get_https_proxy(c), "localhost" );
-    ck_assert_int_eq( pb_config_get_timeout(c), 0 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/proxies.json") , ==, 0);
+    g_assert( pb_config_get_token_key(c) == NULL );
+    g_assert( pb_config_get_https_proxy(c) != NULL );
+    g_assert_cmpint( strlen(pb_config_get_https_proxy(c)), !=, 0 );
+    g_assert_cmpstr( pb_config_get_https_proxy(c), ==, "localhost" );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  0 );
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/timeout.json"), 0);
-    ck_assert( pb_config_get_token_key(c) == NULL );
-    ck_assert( pb_config_get_https_proxy(c) == NULL );
-    ck_assert_int_eq( pb_config_get_timeout(c), 2 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/timeout.json") , ==, 0);
+    g_assert( pb_config_get_token_key(c) == NULL );
+    g_assert( pb_config_get_https_proxy(c) == NULL );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  2 );
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/token_key.json"), 0);
-    ck_assert( pb_config_get_https_proxy(c) == NULL );
-    ck_assert( pb_config_get_token_key(c) != NULL );
-    ck_assert_int_ne( strlen(pb_config_get_token_key(c)), 0 );
-    ck_assert_str_eq( pb_config_get_token_key(c), "toto_is_in_da_place!" );
-    ck_assert_int_eq( pb_config_get_timeout(c), 0 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/token_key.json") , ==, 0);
+    g_assert( pb_config_get_https_proxy(c) == NULL );
+    g_assert( pb_config_get_token_key(c) != NULL );
+    g_assert_cmpint( strlen(pb_config_get_token_key(c)), !=, 0 );
+    g_assert_cmpstr( pb_config_get_token_key(c), ==, "toto_is_in_da_place!" );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  0 );
     pb_config_unref(c);
 
     c = pb_config_new();
-    ck_assert( c != NULL );
-    ck_assert_int_eq(pb_config_from_json_file(c, "conf/all.json"), 0);
-    ck_assert( pb_config_get_https_proxy(c) != NULL );
-    ck_assert_int_ne( strlen(pb_config_get_https_proxy(c)), 0 );
-    ck_assert_str_eq( pb_config_get_https_proxy(c), "localhost" );
-    ck_assert( pb_config_get_token_key(c) != NULL );
-    ck_assert_int_ne( strlen(pb_config_get_token_key(c)), 0 );
-    ck_assert_str_eq( pb_config_get_token_key(c), "toto_is_in_da_place!" );
-    ck_assert_int_eq( pb_config_get_timeout(c), 2 );
+    g_assert( c != NULL );
+    g_assert_cmpint(pb_config_from_json_file(c, "conf/all.json") , ==, 0);
+    g_assert( pb_config_get_https_proxy(c) != NULL );
+    g_assert_cmpint( strlen(pb_config_get_https_proxy(c)), !=, 0 );
+    g_assert_cmpstr( pb_config_get_https_proxy(c), ==, "localhost" );
+    g_assert( pb_config_get_token_key(c) != NULL );
+    g_assert_cmpint( strlen(pb_config_get_token_key(c)), !=, 0 );
+    g_assert_cmpstr( pb_config_get_token_key(c), ==, "toto_is_in_da_place!" );
+    g_assert_cmpint( pb_config_get_timeout(c), ==,  2 );
     pb_config_unref(c);
 }
-END_TEST
 
-// pb_config_from_json_file
 
-Suite * pb_config_suite(void)
+int main (int argc, char *argv[])
 {
-    Suite *s;
-    TCase *tc_core;
+    g_test_init (&argc, &argv, NULL);
 
-    s = suite_create("Config");
+    g_test_add_func("/user/empty-config", test_empty_config);
+    g_test_add_func("/user/set-get-https-proxy", test_https_proxy);
+    g_test_add_func("/user/set-get-timeout", test_timeout);
+    g_test_add_func("/user/set-get-token-key", test_token_key);
+    g_test_add_func("/user/from-json-file", test_from_json_file);
 
-    /* Core test case */
-    tc_core = tcase_create("Core");
-
-    tcase_add_test(tc_core, test_pb_config_new);
-    tcase_add_test(tc_core, test_pb_config_ref_unref);
-    tcase_add_test(tc_core, test_pb_config_https_proxy);
-    tcase_add_test(tc_core, test_pb_config_timeout);
-    tcase_add_test(tc_core, test_pb_config_token_key);
-    tcase_add_test(tc_core, test_pb_config_from_json_file);
-
-    suite_add_tcase(s, tc_core);
-
-    return s;
+    return g_test_run ();
 }
---
+++ tests/ts_user.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+#include "pushbullet.h"
+
+static void test_empty_user(void)
+{
+    pb_user_t* u = pb_user_new();
+
+    g_assert( u != NULL );
+    
+    pb_user_free(u);
+}
+
+static void test_config_to_user(void)
+{
+    pb_user_t* u = pb_user_new();
+    pb_config_t* c1 = NULL;
+    pb_config_t* c2 = NULL;
+
+    g_assert( u != NULL );
+    
+    g_assert( pb_user_get_config(u) == NULL );
+    g_assert( pb_user_set_config(u, c1) == -1 );
+
+    c1 = pb_config_new();
+    pb_config_from_json_file(c1, "conf/all.json");
+    g_assert( pb_user_set_config(u, c1) == 0 );
+
+    c2 = pb_user_get_config(u);
+    g_assert( c2 != NULL );
+    g_assert( c1 == c2 );
+
+    pb_user_free(u);
+}
+
+// http_code_t pb_user_get_info(pb_user_t *p_user);
+// pb_device_t* pb_user_get_devices_list(const pb_user_t* p_user);
+
+int main (int argc, char *argv[])
+{
+    g_test_init (&argc, &argv, NULL);
+
+    g_test_add_func("/user/empty-user", test_empty_user);
+    g_test_add_func("/user/config-to_user", test_config_to_user);
+
+    return g_test_run ();
+}
